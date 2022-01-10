efcfb0da6e8a304c6f2dc585d7686b522e0ec9d5
Charlie <ruggedcafe@gmail.com>
2022-01-10 22:12:35 +0900

    feat: PART 6 완료

---
+++ edu/src/com/edu/part6/SendRedirectTestServlet.java
@@ -0,0 +1,30 @@
+package com.edu.part6;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/portalSite")
+public class SendRedirectTestServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String param = req.getParameter("site");
+		if (param.equals("naver")) {
+			resp.sendRedirect("https://www.naver.com");
+		}
+		if (param.equals("daum")) {
+			resp.sendRedirect("https://www.daum.net");
+		}
+		if (param.equals("zum")) {
+			resp.sendRedirect("https://zum.com");
+		}
+		if (param.equals("google")) {
+			resp.sendRedirect("https://www.google.com");
+		}
+	}
+}
